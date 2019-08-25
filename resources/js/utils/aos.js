6d478e80ff04f41b00db0c0dfbc66a0aadeb3cbb
= <=>
2019-08-25 14:42:36 +0200

    removed old project files

--- resources/js/utils/aos.js
+++
@@ -1,9 +0,0 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
-AOS.init({
-  offset: 100,
-  duration: 700,
-  easing: "ease-in-sine",
-  delay: 100,
-  once: true
-});
--- resources/js/utils/iconsLibrary.js
+++
@@ -1,8 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faPhoneVolume,
-  faAt,
-  faFax,
-  faArrowUp
-} from "@fortawesome/free-solid-svg-icons";
-library.add(faPhoneVolume, faAt, faFax, faArrowUp);
